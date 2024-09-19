407f86aef1469bc9da66f75f6adc64cc226201e4
Tara Ghose <taraghose@Taras-MacBook-Pro-2.local>
2024-09-19 13:20:37 -0400

    Created madlib file

---
+++ madlib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear 
+echo "Let's build a mad-lib!"
+
+read -p "1. Name a type of animal: " NOUN1
+read -p "1. Name a type of place: " NOUN2
+read -p "1. Name an action word: " VERB1
+read -p "1. Name another action word: " VERB2
+read -p "1. Name a word to describe your best friend: " ADJ1
+read -p "1. Name a word to descibe your worst enemy: " ADJ2 
+read -p "1. Name a word to describe when something happened (tomorrow, 
+today, soon etc): " ADV1
+read -p "1. Name a word to describe how often something happens: " ADV2
+
+echo "Once upon a time a $NOUN1 smelled something funny. He looked around 
+and could not tell where the stench was coming from so he searched the 
+NOUN2." 
+
+
+
+
+
+
