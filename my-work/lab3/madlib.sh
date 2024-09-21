d30fd3dad3c5335cd6c0d8ddf93b7f6f19b67c29
taraghose <ghosetara@gmail.com>
2024-09-21 15:13:40 -0400

    Add lab3 files to my-work/lab3

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear 
+echo "Let's build a mad-lib!"
+
+read -p "1. Name a type of animal: " NOUN1
+read -p "1. Name a type of place: " NOUN2
+read -p "1. Name an action word: " VERB1
+read -p "1. Name another action word: " VERB2
+read -p "1. Name a word to describe your best friend: " ADJ1
+read -p "1. Name a word to descibe your worst enemy: " ADJ2 
+read -p "1. Name a word to describe when something happened (tomorrow, 
+today, yesterday): " ADV1
+read -p "1. Name a word to describe how often something happens: " ADV2
+
+echo "Once upon a time a $NOUN1 smelled something funny. He looked around 
+and could not tell where the stench was coming from so he searched the 
+$NOUN2. While he was $VERB1, he tripped over a $ADJ2 rock. The rock had 
+magical powers and started talking to the $NOUN1. The rock told the $NOUN1 
+that he was cursed and in order to be a $ADJ1 $NOUN he had to cast a spell 
+$ADV2. The $NOUN1 was scared and started $VERB2. He ended up waking up 
+from his dream. This all happened $ADV1 and the $NOUN1 will never get past 
+this event."  
+
+
+
+
+
+
